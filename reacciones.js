e2cb950efee07908994e2f6b5aa279f1c470c6a7
AlexisRivero-T <alexisleticiajacob@gmail.com>
2026-07-13 00:00:08 -0300

    Update Firebase database references and initialization

--- reacciones.js
+++ reacciones.js
@@ -1,29 +1,172 @@
+// reacciones.js
+
 import { db } from "./firebase.js";
 
 import {
-  ref,
-  runTransaction,
-  onValue
-} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-database.js";
+    ref,
+    onValue,
+    runTransaction,
+    set
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
-window.addReaction = function(tipo){
 
-    const contador = ref(db,"reacciones/"+tipo);
 
-    runTransaction(contador,(valor)=>{
-        return (valor || 0) + 1;
-    });
+// REFERENCIAS
+
+const loveRef =
+ref(db,"reacciones/love");
+
+
+const shockRef =
+ref(db,"reacciones/shock");
+
+
+const fireRef =
+ref(db,"reacciones/fire");
+
+
+
+
+// CREAR VALORES INICIALES SI NO EXISTEN
+
+
+function inicializarReaccion(referencia, valorInicial){
+
+
+runTransaction(
+referencia,
+(valor)=>{
+
+
+if(valor === null){
+
+    return valorInicial;
 
 }
 
-["love","shock","fire"].forEach(tipo=>{
 
-    onValue(ref(db,"reacciones/"+tipo),(snap)=>{
+return valor;
 
-        const valor=snap.val()||0;
-
-        document.getElementById("count-"+tipo).innerText=valor.toLocaleString();
-
-    });
 
 });
+
+
+}
+
+
+
+inicializarReaccion(
+loveRef,
+250000
+);
+
+
+inicializarReaccion(
+shockRef,
+150000
+);
+
+
+inicializarReaccion(
+fireRef,
+87000
+);
+
+
+
+
+
+// FORMATO DE NÚMEROS
+
+
+function formato(numero){
+
+return Number(numero)
+.toLocaleString("es-UY");
+
+}
+
+
+
+
+
+// MOSTRAR CONTADORES
+
+
+onValue(
+loveRef,
+(snapshot)=>{
+
+
+document.getElementById("count-love").innerHTML =
+
+formato(snapshot.val() || 250000);
+
+
+});
+
+
+
+
+
+onValue(
+shockRef,
+(snapshot)=>{
+
+
+document.getElementById("count-shock").innerHTML =
+
+formato(snapshot.val() || 150000);
+
+
+});
+
+
+
+
+
+onValue(
+fireRef,
+(snapshot)=>{
+
+
+document.getElementById("count-fire").innerHTML =
+
+formato(snapshot.val() || 87000);
+
+
+});
+
+
+
+
+
+
+
+// FUNCIÓN GLOBAL PARA LOS BOTONES HTML
+
+
+window.addReaction = function(tipo){
+
+
+
+const referencia =
+
+ref(db,"reacciones/"+tipo);
+
+
+
+
+
+runTransaction(
+referencia,
+(valorActual)=>{
+
+
+return (valorActual || 0) + 1;
+
+
+});
+
+
+};
